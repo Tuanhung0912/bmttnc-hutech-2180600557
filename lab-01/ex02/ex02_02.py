dd252df02672f65a4b48f9f1b682a6febfa22edf
tuanhung0912 <tuanhung0912@gmail.com>
2024-04-03 15:30:15 +0700

    [add] lab-01 ex01 ex02

---
+++ lab-01/ex02/ex02_02.py
@@ -0,0 +1,3 @@
+ban_kinh = float(input("Nhập Bán Kính của hình tròn: "))
+dien_tich = 3.14 * (ban_kinh ** 2)
+print("Diện Tích của Hình Tròn Là: ", dien_tich)
---
+++ lab-01/ex02/ex02_03.py
@@ -0,0 +1,5 @@
+so = int(input("Nhập vào một số nguyên: "))
+if so % 2 == 0:
+    print(so, "là Số Chẵn")
+else:
+    print(so, "Không Phải Là Số Chẵn")
